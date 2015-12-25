fa9b56dda5aa1baee3ac93a403ab8f5430f12f47
aseddiki <anass.seddiki@kapptivate.com>
2015-12-25 19:05:52 +0100

    First commit for the java version of the spark facebook app

---
+++ src/main/java/com/t3g7/spark/facebook/App.java
@@ -0,0 +1,11 @@
+package com.t3g7.spark.facebook;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main( String[] args ) {
+        System.out.println( "Hello World!" );
+    }
+}
