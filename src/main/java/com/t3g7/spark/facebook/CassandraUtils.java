848d7f552b45fa606220608b1f639de71e44a0e2
aseddiki <anass.seddiki@kapptivate.com>
2016-01-03 16:13:58 +0100

    Cassandra and Spark added in the app as it is in the twitter app, todo : adapt it to facebook context

---
+++ src/main/java/com/t3g7/spark/facebook/CassandraUtils.java
@@ -0,0 +1,51 @@
+package com.t3g7.spark.facebook;
+
+import com.datastax.driver.core.Session;
+import com.datastax.spark.connector.cql.CassandraConnector;
+
+import org.apache.spark.SparkConf;
+
+public class CassandraUtils {
+	/**
+	 * Class model is in the project Spark-Streaming-Twitter in file
+	 * utils/CassandraSettings.scala
+	 */
+	private CassandraUtils() {
+		// TODO Auto-generated constructor stub
+	}
+
+	private static CassandraUtils INSTANCE = new CassandraUtils();
+
+	public static CassandraUtils getInstance() {
+		return INSTANCE;
+	}
+
+	public void setUp(SparkConf conf) {
+		CassandraConnector connector = CassandraConnector.apply(conf);
+
+		// TODO : Adapt the fields of the table created to the facebook data template
+		try (Session session = connector.openSession()) {
+			session.execute("CREATE KEYSPACE IF NOT EXISTS facebook_streaming WITH REPLICATION = {'class': 'SimpleStrategy', 'replication_factor': 1}");
+			session.execute("CREATE TABLE IF NOT EXISTS facebook_streaming.tweets ("
+					+ "body text,"
+					+ "id bigint,"
+					+ "user_screen_name text,"
+					+ "lang text,"
+					+ "created_at timestamp,"
+					+ "favorite_count int,"
+					+ "retweet_count int,"
+					+ "tweet_id bigint,"
+					+ "user_mentions list<text>,"
+					+ "reply_id bigint,"
+					+ "response_time text,"
+					+ "hashtags list<text>,"
+					+ "urls list<text>,"
+					+ "sentiment text,"
+					+ "PRIMARY KEY (body, user_id, tweet_id, user_screen_name, sentiment))");
+			session.execute("CREATE TABLE IF NOT EXISTS twitter_streaming.freq ("
+					+ "date timestamp,"
+					+ "count counter,"
+					+ "PRIMARY KEY (date))");
+		}
+	}
+}
--- src/main/java/com/t3g7/spark/facebook/FacebookStreamingApp.java
+++ src/main/java/com/t3g7/spark/facebook/FacebookStreamingApp.java
@@ -2,14 +2,25 @@ package com.t3g7.spark.facebook;
 
 import java.util.Timer;
 
+import org.apache.spark.SparkConf;
+import org.apache.spark.SparkContext;
+
 public class FacebookStreamingApp {
 	public static void main(String[] args) {
+		CassandraUtils cassandraUtils = CassandraUtils.getInstance();
 		FacebookUtils facebookUtils = FacebookUtils.getInstance();
-		
+
+		// Set Spark configuration and context
+		SparkConf conf = new SparkConf().setMaster("local[2]")
+				.setAppName("FacebookStreamingApp")
+				.set("spark.cassandra.connection.host", "localhost");
+		SparkContext sc = new SparkContext(conf);
+
+		cassandraUtils.setUp(conf);
 		facebookUtils.facebookConfig();
 
 		Timer timer = new Timer();
-		// Beware : values below in miliseconds 
+		// Beware : values below in miliseconds
 		timer.scheduleAtFixedRate(new Streamer(), 0, 15 * 1000);
 	}
 }
